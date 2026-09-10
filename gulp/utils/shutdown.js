bb71148ff730d6f8ff3e8cd330ca27d1fa5313da
Alex Loskutov <599181+aloskutov@users.noreply.github.com>
2026-09-10 22:38:40 +0700

    add: shutdown.js util

---
+++ gulp/utils/shutdown.js
@@ -0,0 +1,44 @@
+// gulp/utils/shutdown.js
+import process from 'node:process';
+
+import { bs } from '../config/browser-sync.js';
+import { watchers } from './watcher.js';
+
+let isShuttingDown = false;
+
+const shutdown = (signal) => {
+  // Защита от повторного вызова (Ctrl+C несколько раз)
+  if (isShuttingDown) return;
+  isShuttingDown = true;
+
+  console.log(`\n📴 Received ${signal}, shutting down gracefully...`);
+
+  // 1. Закрываем watcher'ы
+  watchers.forEach((w) => {
+    try {
+      w.close();
+    } catch {
+      // ignore
+    }
+  });
+  watchers.length = 0;
+
+  // 2. Останавливаем BrowserSync
+  try {
+    if (bs.active) bs.exit();
+  } catch {
+    // ignore
+  }
+
+  // 3. Даём задачам завершиться и выходим
+  setTimeout(() => process.exit(0), 100);
+};
+
+/**
+ *  Явная регистрация shutdown'а
+ */
+export function registerShutdown() {
+  process.on('SIGINT', () => shutdown('SIGINT'));
+  process.on('SIGTERM', () => shutdown('SIGTERM'));
+  process.on('SIGHUP', () => shutdown('SIGHUP'));
+}
--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,9 @@ import { webpack } from './gulp/tasks/webpack.js';
 import { images } from './gulp/tasks/images.js';
 
 import { server } from './gulp/utils/server.js';
+import { registerShutdown } from './gulp/utils/shutdown.js';
+
+registerShutdown();
 
 const build = series(
   clean,
