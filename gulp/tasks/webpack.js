943296d034ce8c77c10c1cf054f0c93a1a7321af
Alex Loskutov <599181+aloskutov@users.noreply.github.com>
2026-09-10 22:36:46 +0700

    update: webpack task

--- gulp/tasks/webpack.js
+++ gulp/tasks/webpack.js
@@ -1,17 +1,37 @@
+// gulp/tasks/webpack.js
 import { paths } from '../config/paths.js';
 import { options } from '../config/options.js';
+import { bs } from '../config/browser-sync.js';
 
 import gulp from 'gulp';
 const { src, dest } = gulp;
 
 import plumber from 'gulp-plumber';
 import webpackStream from 'webpack-stream';
+import webpack from 'webpack';
 
-const webpack = () => {
+const webpackTask = () => {
   return src(paths.src.jsEntry)
     .pipe(plumber())
-    .pipe(webpackStream(options.webpack))
-    .pipe(dest(paths.build.js));
+    .pipe(
+      webpackStream(options.webpack, webpack, (err, stats) => {
+        if (err) {
+          console.error('Webpack fatal error:', err);
+          return;
+        }
+        if (stats?.hasErrors()) {
+          console.error(
+            stats.toString({
+              colors: true,
+              chunks: false,
+              modules: false,
+            })
+          );
+        }
+      })
+    )
+    .pipe(dest(paths.build.js))
+    .pipe(bs.stream());
 };
 
-export { webpack };
+export { webpackTask as webpack };
