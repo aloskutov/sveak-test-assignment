bd6c5ee5509c09f0d622f72ee6f644ece2e3cc4b
Alex Loskutov <599181+aloskutov@users.noreply.github.com>
2026-09-11 11:46:09 +0700

    add: gulp-imagemin plugin

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -1,16 +1,35 @@
-import {paths} from '../config/paths.js';
+import { paths } from '../config/paths.js';
 import { bs } from '../config/browser-sync.js';
 
 import gulp from 'gulp';
-const {src, dest} = gulp;
+const { src, dest } = gulp;
 
 import plumber from 'gulp-plumber';
+import imagemin, { gifsicle, mozjpeg, optipng, svgo } from 'gulp-imagemin';
+
 
 const images = () => {
-  return src(paths.src.images, {encoding: false})
+  return src(paths.src.images, { encoding: false })
     .pipe(plumber())
+    .pipe(imagemin([
+      gifsicle({ interlaced: true }),
+      mozjpeg({ quality: 85, progressive: true }),
+      optipng({ optimizationLevel: 5 }),
+      svgo({
+        plugins: [
+          {
+            name: 'removeViewBox',
+            active: true
+          },
+          {
+            name: 'cleanupIDs',
+            active: false
+          }
+        ]
+      })
+    ]))
     .pipe(dest(paths.build.images))
     .pipe(bs.stream());
 };
 
-export {images};
+export { images };
