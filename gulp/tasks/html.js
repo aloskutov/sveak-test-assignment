221b664387f9bf89112742eb0b803875c4320244
Alex Loskutov <599181+aloskutov@users.noreply.github.com>
2026-09-11 15:13:56 +0700

    add: asset helper

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,38 +1,43 @@
-import {paths} from '../config/paths.js';
-import {options} from '../config/options.js';
+import { paths } from '../config/paths.js';
+import { options } from '../config/options.js';
 import { bs } from '../config/browser-sync.js';
 
 import gulp from 'gulp';
-const {src, dest} = gulp;
+const { src, dest } = gulp;
 
 import pug from 'gulp-pug';
 import plumber from 'gulp-plumber';
 
-import spellsData     from '../../data/spells.json' with { type: "json" };
-import menuData       from '../../data/menu.json' with { type: "json" };
+import spellsData from '../../data/spells.json' with { type: "json" };
+import menuData from '../../data/menu.json' with { type: "json" };
 
 const html = () => {
-    return src(paths.build.pug, { allowEmpty: true })
-      .pipe(
-        plumber({
-          errorHandler: function (err) {
-            console.error('PUG error:', err.message);
-            this.emit('end');
+  return src(paths.build.pug, { allowEmpty: true })
+    .pipe(
+      plumber({
+        errorHandler: function (err) {
+          console.error('PUG error:', err.message);
+          this.emit('end');
+        },
+      })
+    )
+    .pipe(
+      pug({
+        ...options.pug,
+        locals: {
+          isProd: options.isProd,
+          asset: (name, ext) => {
+            const suffix = options.isProd ? '.min' : '';
+            const folder = ext === 'css' ? 'css' : ext === 'js' ? 'js' : ext;
+            return `/assets/${folder}/${name}${suffix}.${ext}`;
           },
-        })
-      )
-      .pipe(
-        pug({
-          ...options.pug,
-          locals:{
-            isProd: options.isProd,
-            menu: menuData,
-            spells: spellsData
-          }
-        })
-      )
-      .pipe(dest(paths.build.html))
-      .pipe(bs.stream());
+          menu: menuData,
+          spells: spellsData
+        }
+      })
+    )
+    .pipe(dest(paths.build.html))
+    .pipe(bs.stream());
 };
 
-export {html};
+export { html };
