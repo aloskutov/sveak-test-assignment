46ba6d3c0b5470a94592a1da25cba62728af224e
Alex Loskutov <599181+aloskutov@users.noreply.github.com>
2026-09-11 22:02:09 +0700

    update: eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,21 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 import jsdoc from 'eslint-plugin-jsdoc';
 
 export default [
-  {languageOptions: {
-    globals: globals.browser,
-    ecmaVersion: 2026,
-    sourceType: "module" }
+  {
+    languageOptions: {
+      globals: globals.browser,
+      ecmaVersion: 2026,
+      sourceType: 'module',
+    },
   },
   pluginJs.configs.recommended,
-  {
-    files: ['**/*.js'],
-    plugins: {
-      jsdoc,
-    },
-    rules: {
-      'jsdoc/require-description': 'warn'
-    }
-  },
   jsdoc.configs['flat/recommended'],
+  {
+    files: ['src/assets/js/**/*.js'],
+    rules: {
+      'jsdoc/require-description': 'warn',
+    },
+  },
 ];
