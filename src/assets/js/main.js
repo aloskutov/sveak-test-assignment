f77d5b0bc6bcb5908654954f5a69c12da70c6d30
Alex Loskutov <599181+aloskutov@users.noreply.github.com>
2026-09-22 23:33:38 +0700

    refactoring: pug

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,8 +1,8 @@
 import AdjustLastRow from './modules/adjust-last-row.js';
 import Hamburger from './modules/hamburger.js';
 
-new AdjustLastRow('.cards', '.card');
-new Hamburger('.hamburger', '#top-menu', {
+new AdjustLastRow('.cards', '.cards__item');
+new Hamburger('.hamburger', '#side-menu', {
   openLabel: 'Open menu',
   closeLabel: 'Close menu',
   scrollToTop: true
